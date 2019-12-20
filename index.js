9dd74079dd85887e0785376187bf3204b8e74af6
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-12-21 00:10:07 +0300

    #10 SessionStorage was added

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ var Storager = {
         },
         remove: function(key) {
             localStorage.removeItem(key);
+        },
+        clear: function() {
+            localStorage.clear()
+        }
+    },
+    SessionStorage: {
+        set: function(key, value) {
+            sessionStorage.setItem(key, value)
+        },
+        get: function(key) {
+            return sessionStorage.getItem(key);
+        },
+        setObject: function(key, value) {
+            sessionStorage.setItem(key, JSON.stringify(value));
+        },
+        getObject: function(key) {
+            return JSON.parse(sessionStorage.getItem(key));
+        },
+        remove: function(key) {
+            sessionStorage.removeItem(key);
+        },
+        clear: function() {
+            sessionStorage.clear()
         }
     }
 };
