096ea86f556d849d86c57eda9b1460f2e0a14a68
Andrei Arkhipov <andrei@awb.pw>
2020-03-01 00:47:41 +0300

    #43: Handle a situation where the storage is not available

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import CookieComponent from './components/Cookie';
 import StorageComponent from './components/Storage';
 
 export const Cookie = new CookieComponent(document.cookie);
-export const LocalStorage = new StorageComponent(localStorage);
-export const SessionStorage = new StorageComponent(sessionStorage);
+export const LocalStorage = typeof localStorage !== 'undefined' ? new StorageComponent(localStorage) : null;
+export const SessionStorage = typeof sessionStorage !== 'undefined' ? new StorageComponent(sessionStorage) : null;
