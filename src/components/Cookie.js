b0ef407272a8de1d5ba7b8677bb41a05b5a861ea
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2022-06-25 13:19:40 +0200

    Fix(cookie): The problem with writing to cookie has been solved

--- src/components/Cookie.js
+++ src/components/Cookie.js
@@ -1,23 +1,21 @@
-export default class Cookie {
-  constructor(cookie) {
-    this.cookie = cookie;
-  }
-
+const Cookie = {
   get(name) {
-    const matches = this.cookie.match(new RegExp(
+    const matches = document.cookie.match(new RegExp(
       `(?:^|; )${name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1')}=([^;]*)`,
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
+  },
 
   set(name, value, days) {
     const d = new Date();
     d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
     const expires = `expires=${d.toUTCString()}`;
-    this.cookie = `${name}=${value};${expires};path=/`;
-  }
+    document.cookie = `${name}=${value};${expires};path=/`;
+  },
 
   remove(name) {
     this.set(name, '', -1);
-  }
-}
+  },
+};
+
+export default Cookie;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import CookieComponent from './components/Cookie';
 import StorageComponent from './components/Storage';
 
-export const Cookie = new CookieComponent(document.cookie);
+export const Cookie = CookieComponent;
 export const LocalStorage = typeof localStorage !== 'undefined' ? new StorageComponent(localStorage) : null;
 export const SessionStorage = typeof sessionStorage !== 'undefined' ? new StorageComponent(sessionStorage) : null;
