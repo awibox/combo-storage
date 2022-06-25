d153649d8ed43f669d26841d4a9b7bcf9d8ba1ae
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2022-06-25 13:34:56 +0200

    fix(cookie): The problem with writing to cookie has been solved

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# [2.1.0](https://github.com/awibox/combo-storage/compare/v2.0.6...v2.1.0) (2022-06-24)
-
-
-### Features
-
-* **docs:** workflow ([a497baf](https://github.com/awibox/combo-storage/commit/a497bafa34ac7c35dd4bf3675f13a89acd2e4d9e))
--- specs/Cookie.spec.js
+++ specs/Cookie.spec.js
@@ -2,17 +2,17 @@ import Cookie from '../src/components/Cookie';
 
 const testKey = 'testCookie';
 
-const cookie = 'testKey=testCookie; path=/; max-age=3600; secure; domain=domain.com';
+document.cookie = 'testKey=testCookie; path=/; max-age=3600; secure; domain=domain.com';
 
 describe('Cookie', () => {
   it('Cookie flow', () => {
-    const component = new Cookie(cookie);
+    const component = Cookie;
     component.set('testKey', testKey, 60);
     expect(component.get('testKey')).toEqual(testKey);
     component.remove('testKey');
   });
   it('Cookie undefined', () => {
-    const component = new Cookie(cookie);
+    const component = Cookie;
     expect(component.get('undefinedKey')).toEqual(undefined);
   });
 });
